a5dea00c00f6d31243f32157d3fbd1036fe63a95
hadi-a-azeez <hadikoduvally@gmail.com>
2020-12-30 21:00:24 +0530

    binary search tree

---
+++ 20.bst.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int data;
+    struct node *leftChild;
+    struct node *rightChild;
+}*root = NULL;
+
+void insertToBst(int data){
+    struct node *tempnode,*current,*parent;
+    tempnode = (struct node*) malloc(sizeof(struct node));
+    tempnode->data = data;
+    tempnode->leftChild = NULL;
+    tempnode->rightChild = NULL;
+
+    if(root == NULL){
+        root = tempnode;
+        printf("\n Inserted at root of the bts");
+    }
+    else{
+        current = root;
+        parent = NULL;
+
+        while(1){
+            parent = current; //changing the parent for every iteration. ie, current is changing
+
+            //checkig to the left side of the tree
+            if(current->data > data){
+                current = current->leftChild;
+
+                if(current == NULL){
+                    parent->leftChild = tempnode; //inserting to left
+                    printf("\nInserted at left of the bts");
+                    return;
+                }
+            }
+            //checking to right
+            else{
+                current = current->rightChild;
+
+                if(current == NULL){
+                    parent->rightChild = tempnode; //inserting to right
+                    printf("\nInserted at the right of the bts");
+                    return;
+                }
+            }
+        } // while loop will always be true(since the condition is true)
+         //for each iteration current is changing
+         //loop will terminate only if insertion completes and return from fun
+    }
+}
+
+struct node* searchBts(int item){
+    struct node *current;
+    current = root;
+
+    //checking if the root is null
+     if(current == NULL){
+            return NULL;
+        }
+           
+     while(current->data != item){  //checking for each iteration if the item is present
+        //check to the left
+        if(current->data > item){
+            current = current->leftChild; //changing current
+        }
+        //checking to the right
+        else{
+            current = current->rightChild; //changing current
+        }
+        if(current == NULL){  //
+            return NULL;
+        }   
+    }
+    return current;
+}
+
+int main(){
+    int data,item;
+    struct node *result;
+    char ch;
+    int choice;
+    while(choice != 5){
+        printf("\n\nChoose from menu\n");
+        printf("-----------------\n");
+        printf("1. Insert to bts\n2. Search in bts\n3.Insert at random position\n4. Display\n5. Exit\n");
+        printf("Select a option:");
+        scanf("%d",&choice);
+        switch (choice)
+        {
+        case 1:
+            printf("\nEnter data to bts:");
+            scanf("%d",&data);
+            insertToBst(data);
+            break;
+        case 2:
+            printf("\nEnter the data to be searched in the bts:");
+            scanf("%d",&item);
+            result = searchBts(item);
+            if(result == NULL){
+                printf("\nNot found");
+            }
+            else{
+                printf("\n item present in bst");
+            }
+            break; 
+       /*  case 3:
+            insertRandom();
+            break;
+        case 4:
+            display();
+            break; */
+        case 5:
+            return(0);
+            break;
+        
+        default:
+            printf("Print a valid option\n");
+            break;
+        }
+    }  
+}
